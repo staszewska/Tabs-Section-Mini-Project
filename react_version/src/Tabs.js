469eec3c985a65768857a59e700a548c527e7bc4
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2023-12-16 15:47:32 +0100

    inital setup

---
+++ react_version/src/Tabs.js
@@ -0,0 +1,37 @@
+
+
+export default function Tabs() {
+
+    return (
+    <section className="container example">
+        <div className="img__container" />
+        <div className="tabs">
+            <div className="buttons__container">
+                <button className="button1">Step 1</button>
+                <button className="button2">Step 2</button>
+                <button className="button3">Step 3</button>
+            </div>
+            <div className="tabs__container">
+                <div className="tabs__content1">
+                    <h1>Step 1</h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, nisi. Sint maxime
+                    vitae laudantium vero ea. Ea ex at ut quos molestias, eum et quis repellendus consequatur voluptas
+                    libero consectetur?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nesciunt, sint temporibus possimus minus a at eveniet voluptatum mollitia, voluptas adipisci, nulla nam corporis itaque. Tempore fugiat officia illum id?
+                </div>
+                <div className="tabs__content2">
+                    <h1>Step 2</h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo beatae quia esse
+                    quae dolor enim quaerat laboriosam, distinctio, velit eum ut inventore exercitationem pariatur
+                    dolores deserunt unde earum, voluptatem fugiat!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero expedita amet temporibus deserunt facere autem labore, corporis reprehenderit praesentium consequatur aliquid laboriosam iusto neque illum eaque quibusdam minus. Deserunt, enim.
+                </div>
+                <div className="tabs__content3">
+                    <h1>Step 3</h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, maxime sunt
+                    laborum voluptatibus sequi repellat quam, modi aliquam magni quibusdam nulla perferendis, quo cum
+                    molestias eveniet asperiores sint earum iste.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus esse nemo facere quia repellat cupiditate nam sapiente? Rem dicta distinctio molestias, excepturi sint minus dolorum beatae rerum recusandae, at atque.
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
